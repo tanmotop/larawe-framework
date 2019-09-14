2d39c820f7688ff30b95229fc5d5b8fad4b7e697
洪宝同 <410536770@qq.com>
2019-09-14 22:47:36 +0800

    Resolve the redirect bad uri

---
+++ src/Routing/Redirector.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanmo
+ * Date: 2019/9/14
+ * Time: 10:24 PM
+ */
+
+namespace Tanmo\Wq\Routing;
+
+use Illuminate\Routing\Redirector as IlluminateRedirector;
+use Illuminate\Support\Str;
+
+class Redirector extends IlluminateRedirector
+{
+    /**
+     * 为了避免生成的URI中出现两个(?) 而重写的函数
+     * @param string $path
+     * @param int $status
+     * @param array $headers
+     * @param null $secure
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function to($path, $status = 302, $headers = [], $secure = null)
+    {
+        $path = Str::replaceLast('?', '&', $path);
+        return parent::to($path, $status, $headers, $secure);
+    }
+}
