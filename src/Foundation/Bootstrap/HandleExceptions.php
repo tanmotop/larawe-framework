5c823b2e09b7cce371c0360d662afc85e77b8e6e
洪宝同 <410536770@qq.com>
2019-11-07 16:10:46 +0800

    修改报错级别

--- src/Foundation/Bootstrap/HandleExceptions.php
+++ src/Foundation/Bootstrap/HandleExceptions.php
@@ -37,7 +37,7 @@ class HandleExceptions
 
         $this->app = $app;
 
-        error_reporting(-1);
+        error_reporting(E_ALL ^ E_NOTICE);
 
         set_error_handler([$this, 'handleError']);
 
